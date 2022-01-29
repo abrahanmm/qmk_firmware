409c57ca83b54a6b724cd5aa3980b352f3e389e6
Abrahán Martínez Martín <abrahammm@ext.inditex.com>
2022-01-29 22:48:01 +0100

    Improve delete line

--- keyboards/lily58/keymaps/bram/keymap.c
+++ keyboards/lily58/keymaps/bram/keymap.c
@@ -450,7 +450,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // SEND_STRING(SS_LCTL("a" SS_LSFT("e")));
         }
         return false;
-    case DELLINE:  // Selects the current line.
+    case DELLINE:  // Delete the current line.
         if (record->event.pressed) {
             SEND_STRING(SS_TAP(X_HOME) SS_TAP(X_HOME) SS_LSFT(SS_TAP(X_END)) SS_TAP(X_DEL) SS_TAP(X_DEL));
             // Mac users, use:
